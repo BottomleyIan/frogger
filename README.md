a4cbf26a252aec1feeb6dfdfcf5119aa2ffbdfba
Ian Bottomley <bottomley.ian@gmail.com>
2026-01-28 18:47:53 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Frogger (Learning Project)
+
+This repository is a personal learning exercise following the Frogger section of
+the Godot YouTube course video at:
+
+https://youtu.be/TLG2yVpLDT8
+
+All credit for the original instruction and content belongs to the course
+creator. This repo is not an original work and is shared only for learning
+purposes.
---
+++ scenes/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+var direction: Vector2
+var speed: int = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta: float) -> void:
+	direction= Input.get_vector("left","right", "up", "down", -1.0)
+	velocity = direction * speed
+	animation()
+	move_and_slide()	
+	
+func animation():
+	if direction:
+		if direction.y > 0:
+			$AnimatedSprite2D.animation = "down"
+		elif direction.y < 0:
+			$AnimatedSprite2D.animation = "up"
+		else:
+			$AnimatedSprite2D.animation = "left"
+			$AnimatedSprite2D.flip_h = direction.x > 0
+		
+	else:
+		$AnimatedSprite2D.frame = 0
